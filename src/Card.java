e2b2bd06804d4e33bdca89d5ffdb0fe3d4c2bc7a
Christian Welch <cw356@duke.edu>
2020-10-26 22:08:04 -0400

    added more code structure

---
+++ src/Card.java
@@ -0,0 +1,37 @@
+public class Card
+{
+    /**
+     * Represents a card in the game, has a value, suit, and visibility.
+     * Visibility determines whether the card can be seen or not on the screen
+     *
+     */
+
+
+    /**
+     * returns value of card (in poker, usually two through ace
+     * @return String
+     */
+    public String getValue(){
+        return "";
+    }
+
+    /**
+     * returns the suit of the card
+     * @return String
+     */
+    public String getSuit(){
+        return "";
+    }
+
+    /**
+     * returns boolean indicating if the card is currently visible to the player or not
+     */
+    public boolean getVisibility(){
+        return false;
+    }
+
+
+
+
+
+}
---
+++ src/Dealer.java
@@ -0,0 +1,38 @@
+public class Dealer {
+    /**
+     * Deals cards to players and deals community cards
+     */
+
+
+    /**
+     * Deals the given number of cards from the deck to the given player
+     * @param player
+     * @param numberToDeal
+     */
+    public void dealCards(Player player, int numberToDeal){
+
+    }
+
+    /**
+     * deals the given number of cards to the communal set on table
+     * @param numberToDeal
+     */
+    //TODO: maybe deal certain cards?? communal cards??
+    public void dealCommunalCards(int numberToDeal){
+
+    }
+
+    /**
+     * discards given number of cards from top of the deck
+     * @param numberToBurn
+     */
+    public void discardCards(int numberToBurn){
+
+    }
+
+
+
+
+
+
+}
