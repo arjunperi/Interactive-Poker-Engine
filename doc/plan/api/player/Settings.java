311785f66cb8afb7bec09c57e0c44a1fa72ccb45
Arjun Peri <ap458@duke.edu>
2020-10-27 00:35:49 -0400

    edited design doc

--- doc/plan/api/player/Settings.java
+++ doc/plan/api/player/Settings.java
@@ -7,7 +7,7 @@ public class Settings{
     /**
      * This method would take in a user input, get the starting amount, save it to a file for future use, and progress the game forward using this input.
      * We plan on allowing the users to access saved settings using login information, and by saving to files we will be able to provide
-     * users with a dynamic user experience and interface that will better emulate a real poker game. 
+     * users with a dynamic user experience and interface that will better emulate a real poker game.
      */
     public void setStartingAmount(){}
 }
