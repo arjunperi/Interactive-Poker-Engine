3a2de297a287edbbb36b68f31a22927a92af9b6d
Yasser <ye9@duke.edu>
2020-11-01 21:50:35 -0500

    Added Suit and Rank to Card

--- src/model/Card.java
+++ src/model/Card.java
@@ -1,13 +1,62 @@
 package model;
 
 public class Card {
-    private int cardValue;
 
-    public Card(int value){
+    public enum Rank {
+        DEUCE,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE
+    }
+
+    public enum Suit {
+        CLUBS,
+        DIAMONDS,
+        HEARTS,
+        SPADES
+    }
+
+    private final Rank rank;
+    private final Suit suit;
+    //private int cardValue;
+
+    /*public Card(int value){
         cardValue = value;
     }
 
     public int getCardValue(){
         return cardValue;
+    }*/
+
+    public Card (Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return (suit + " " + rank);
+    }
+
+    @Override
+    public boolean equals (Object obj) {
+
     }
 }
