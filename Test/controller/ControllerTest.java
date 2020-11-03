2aaf5ccc737f00e328230e95852cec57095486f9
Arjun Peri <ap458@duke.edu>
2020-11-02 22:46:28 -0500

    created testing suite

--- Test/controller/ControllerTest.java
+++ Test/controller/ControllerTest.java
@@ -1,7 +1,49 @@
 package controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javafx.stage.Stage;
+import model.Dealer;
+import model.DealerRules;
+import model.Game;
+import model.TurnManager;
+import org.junit.jupiter.api.Test;
 import util.DukeApplicationTest;
 
+
+
 public class ControllerTest extends DukeApplicationTest {
+
+    private Controller mainController;
+    private DealerRules dealerRules;
+    private TurnManager turnManager;
+    private Game game;
+
+    public void start(final Stage stage) {
+        mainController = new Controller(stage);
+
+    }
+
+
+    @Test
+    public void testGameStep(){
+        dealerRules = game.getDealerRules();
+        turnManager = game.getTurnManager();
+        mainController.gameStep();
+//        assertEquals()
+    }
+
+    @Test
+    public void testDealingRound(){
+
+    }
+
+    @Test
+    public void testGetFrontEndTopCard(){}
+
+    @Test
+    public void initializeFrontEndPlayers(){
+
+    }
 
 }
