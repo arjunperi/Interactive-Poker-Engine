b77b6222efe51ead1b322f2501a8ed9aae493e1d
Christian Welch <cw356@duke.edu>
2020-11-06 14:45:01 -0500

    committing so that I can pull from master, AI player still work in progress

---
+++ src/model/SimulatedPlayer.java
@@ -0,0 +1,13 @@
+package model;
+/*
+Computer-controlled poker player
+ */
+public class SimulatedPlayer extends Player {
+
+    public SimulatedPlayer(String name, int startingAmount, Pot pot) {
+        super(name, startingAmount, pot);
+    }
+
+
+}
+
