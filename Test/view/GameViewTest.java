bdea773d934856e680de28d6e8c954f822ddba2b
Yasser <ye9@duke.edu>
2020-11-19 01:11:21 -0500

    further debugged CardView

--- Test/view/GameViewTest.java
+++ Test/view/GameViewTest.java
@@ -31,31 +31,6 @@ public class GameViewTest extends DukeApplicationTest {
     this.stage.show();
   }
 
-//    @Test
-//    public void testFrontEndDeal() {
-//        CommunityCards communityCards = new CommunityCards();
-//        Pot pot = new Pot();
-//        Player player = new InteractivePlayer("Arjun", 100, communityCards, pot);
-//        player.receiveCard(new Card(14, "DIAMONDS"));
-//        player.receiveCard(new Card(13, "DIAMONDS"));
-//        FrontEndPlayer frontEndPlayer = new FrontEndPlayer(10, 30, "Arjun", 100);
-//        javafxRun(() -> controller.dealFrontEndCardsInRound(player, frontEndPlayer));
-//        testCard = lookup("#ADIAMONDS").query();
-//        assertEquals(20, testCard.getX());
-//        testCard = lookup("#KDIAMONDS").query();
-//        assertEquals(100, testCard.getX());
-//
-//        Player player2 = new InteractivePlayer("Christian", 100, communityCards, pot);
-//        player2.receiveCard(new Card(12, "DIAMONDS"));
-//        player2.receiveCard(new Card(11, "DIAMONDS"));
-//        FrontEndPlayer frontEndPlayer2 = new FrontEndPlayer(10, 80, "Christian", 100);
-//        javafxRun(() -> controller.dealFrontEndCardsInRound(player2, frontEndPlayer2));
-//        testCard = lookup("#QDIAMONDS").query();
-//        assertEquals(20, testCard.getX());
-//        testCard = lookup("#JDIAMONDS").query();
-//        assertEquals(100, testCard.getX());
-//    }
-
   @Test
   public void testStartBox() {
     javafxRun(() -> controller.initializeMainMenu());
@@ -173,64 +148,6 @@ public class GameViewTest extends DukeApplicationTest {
     assertEquals(startingMoneyInput.getText(), "600");
   }
 
-//    @Test
-//    public void testBettingMenu() {
-//        CommunityCards communityCards = new CommunityCards();
-//        Pot pot = new Pot();
-//        Player player = new InteractivePlayer("Arjun", 100, communityCards, pot);
-//        javafxRun(() -> controller.initializeGameSelect());
-//        Button startButton = lookup("#Holdem").queryButton();
-//        clickOn(startButton);
-//        javafxRun(() -> {
-//            controller.displayBetMenu(player);
-//        });
-//        TextField betInput = lookup("#Bet").query();
-//        GridPane optionPane = lookup("#OptionPane").query();
-//        assertTrue(optionPane.getChildren().contains(betInput));
-//    }
-//
-//    @Test
-//    public void testFrontEndExchange() {
-//        CommunityCards communityCards = new CommunityCards();
-//        Pot pot = new Pot();
-//        Player player = new InteractivePlayer("Arjun", 100, communityCards, pot);
-//        Card card1 = new Card(14, "DIAMONDS");
-//        Card card2 = new Card(13, "DIAMONDS");
-//        player.receiveCard(card1);
-//        player.receiveCard(card2);
-//        FrontEndPlayer frontEndPlayer = new FrontEndPlayer(10, 30, "Arjun", 100);
-//        javafxRun(() -> controller.dealFrontEndCardsInRound(player, frontEndPlayer));
-//
-//        player.clearDiscardedCards();
-//        player.clearNewCards();
-//        player.discardCard(card1);
-//        player.discardCard(card2);
-//
-//        Card card3 = new Card(12, "DIAMONDS");
-//        Card card4 = new Card(11, "DIAMONDS");
-//        player.receiveCard(card3);
-//        player.receiveCard(card4);
-//
-//        javafxRun(() -> controller.exchangeFrontEndCards(player, frontEndPlayer));
-//        testCard = lookup("#JDIAMONDS").query();
-//        assertEquals(100, testCard.getX());
-//    }
-
-//  @Test
-//  public void testExchangeInputScreen() {
-//    javafxRun(() -> controller.initializeGameSelect());
-//    Button startButton = lookup("#Holdem").queryButton();
-//    clickOn(startButton);
-//    javafxRun(() -> {
-//      controller.exchangeRound();
-//    });
-//    GridPane exchangeGrid = lookup("#ExchangeGrid").query();
-//    TextField exchangeCardInput1 = lookup("#ExchangeCard1").query();
-//    TextField exchangeCardInput2 = lookup("#ExchangeCard2").query();
-//    TextField exchangeCardInput3 = lookup("#ExchangeCard3").query();
-//    assertTrue(exchangeGrid.getChildren()
-//        .containsAll(List.of(exchangeCardInput1, exchangeCardInput2, exchangeCardInput3)));
-//  }
   
   @Test
   public void testInvalidNumberPlayersExceptionThrown() {
